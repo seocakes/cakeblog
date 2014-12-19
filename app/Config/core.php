265fbb2ca380d6608e53a49146e83b498b39cc6a
seocakes <seocakes@gmail.com>
2014-12-19 22:12:21 +0100

    added -> db_connection changed -> salts

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,13 +222,14 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
-
+//	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'K7ejMbßl3ZnkGtYewJK03md093jnmLDq94bmspm4');
+        
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
-
+//	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '91548765298774394198426442218');
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
  * Will append a query string parameter containing the time the file was modified. This is
